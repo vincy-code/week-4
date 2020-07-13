6911faaf43f775076d2bd09b103d2fb4a254097a
vincy <vincy.emmanuelli@live.com>
2020-07-13 00:53:36 -0400

    update to homework

--- script.js
+++ script.js
@@ -1,44 +1,154 @@
-var myQuestions = [
-	{
-		question: "What is 10/2?",
+(function(){
+	// Functions
+	function buildQuiz(){
+	  
+	  const output = [];
+  
+	  myQuestions.forEach(
+		(currentQuestion, questionNumber) => {
+  
+
+		  const answers = [];
+  
+		  for(letter in currentQuestion.answers){
+  
+			
+			answers.push(
+			  `<label>
+				<input type="radio" name="question${questionNumber}" value="${letter}">
+				${letter} :
+				${currentQuestion.answers[letter]}
+			  </label>`
+			);
+		  }
+  
+		
+		  output.push(
+			`<div class="slide">
+			  <div class="question"> ${currentQuestion.question} </div>
+			  <div class="answers"> ${answers.join("")} </div>
+			</div>`
+		  );
+		}
+	  );
+  
+	  
+	  quizContainer.innerHTML = output.join('');
+	}
+  
+	function showResults(){
+  
+	  
+	  const answerContainers = quizContainer.querySelectorAll('.answers');
+  
+	  
+	  let numCorrect = 0;
+  
+	  
+	  myQuestions.forEach( (currentQuestion, questionNumber) => {
+  
+		
+		const answerContainer = answerContainers[questionNumber];
+		const selector = `input[name=question${questionNumber}]:checked`;
+		const userAnswer = (answerContainer.querySelector(selector) || {}).value;
+  
+		
+		if(userAnswer === currentQuestion.correctAnswer){
+		  
+		  numCorrect++;
+  
+		  
+		  answerContainers[questionNumber].style.color = 'lightgreen';
+		}
+		
+		else{
+		  
+		  answerContainers[questionNumber].style.color = 'red';
+		}
+	  });
+  
+	  // show number of correct answers
+	  resultsContainer.innerHTML = `${numCorrect} out of ${myQuestions.length}`;
+	}
+  
+	function showSlide(n) {
+	  slides[currentSlide].classList.remove('active-slide');
+	  slides[n].classList.add('active-slide');
+	  currentSlide = n;
+	  if(currentSlide === 0){
+		previousButton.style.display = 'none';
+	  }
+	  else{
+		previousButton.style.display = 'inline-block';
+	  }
+	  if(currentSlide === slides.length-1){
+		nextButton.style.display = 'none';
+		submitButton.style.display = 'inline-block';
+	  }
+	  else{
+		nextButton.style.display = 'inline-block';
+		submitButton.style.display = 'none';
+	  }
+	}
+  
+	function showNextSlide() {
+	  showSlide(currentSlide + 1);
+	}
+  
+	function showPreviousSlide() {
+	  showSlide(currentSlide - 1);
+	}
+  
+	// Variables
+	const quizContainer = document.getElementById('quiz');
+	const resultsContainer = document.getElementById('results');
+	const submitButton = document.getElementById('submit');
+	const myQuestions = [
+	  {
+		question: "Who invented JavaScript?",
 		answers: {
-			a: '3',
-			b: '5',
-			c: '115'
+		  a: "Douglas Crockford",
+		  b: "Sheryl Sandberg",
+		  c: "Brendan Eich"
 		},
-		correctAnswer: 'b'
-	},
-	{
-		question: "What is 30/3?",
+		correctAnswer: "c"
+	  },
+	  {
+		question: "Which one of these is a JavaScript package manager?",
 		answers: {
-			a: '3',
-			b: '5',
-			c: '10'
+		  a: "Node.js",
+		  b: "TypeScript",
+		  c: "npm"
 		},
-		correctAnswer: 'c'
-	}
-];
-
-
-
-function generateQuiz(questions, quizContainer, resultsContainer, submitButton){
-
-	function showQuestions(questions, quizContainer){
-		// code will go here
-	}
-
-	function showResults(questions, quizContainer, resultsContainer){
-		// code will go here
-	}
-
-	// show the questions
-	showQuestions(questions, quizContainer);
-
-	// when user clicks submit, show results
-	submitButton.onclick = function(){
-		showResults(questions, quizContainer, resultsContainer);
-	}
-}
-
-
-
+		correctAnswer: "c"
+	  },
+	  {
+		question: "Which tool can you use to ensure code quality?",
+		answers: {
+		  a: "Angular",
+		  b: "jQuery",
+		  c: "RequireJS",
+		  d: "ESLint"
+		},
+		correctAnswer: "d"
+	  }
+	];
+  
+	
+	buildQuiz();
+  
+	
+	const previousButton = document.getElementById("previous");
+	const nextButton = document.getElementById("next");
+	const slides = document.querySelectorAll(".slide");
+	let currentSlide = 0;
+  
+	
+	showSlide(currentSlide);
+  
+	// Event listeners
+	submitButton.addEventListener('click', showResults);
+	previousButton.addEventListener("click", showPreviousSlide);
+	nextButton.addEventListener("click", showNextSlide);
+  })();
+  
